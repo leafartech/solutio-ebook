8708f7fb71cc90efe35f14a3933fd571f9b498ae
leafartech <119262053+leafartech@users.noreply.github.com>
2024-08-29 08:13:40 -0300

    new journey added

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -15,6 +15,7 @@ interface dataProps {
     name: string
     email: string
     phone: string
+    cnpj: string
     utm_term: string
     utm_medium: string
     utm_content: string
@@ -25,6 +26,7 @@ let initialData = {
     name: '',
     email: '',
     phone: '',
+    cnpj: '',
     utm_term: '',
     utm_medium: '',
     utm_content: '',
@@ -68,12 +70,13 @@ export function Form({ utm_campaign, utm_content, utm_medium, utm_source, utm_te
         let dataHlp = data
 
         const formatedData = {
-            "codigoApi": "89CA8DE7B0",
+            "codigoApi": "1FB031DBAB",
             "origemOportunidade": "Integração",
             "lead": {
                 "nomeLead": data.name,
                 "telefoneLead": data.phone,
                 "emailLead": data.email,
+                "cnpjLead": data.cnpj,
                 "origemLead": "Página de captura",
             },
             "contato": {
@@ -145,6 +148,10 @@ export function Form({ utm_campaign, utm_content, utm_medium, utm_source, utm_te
                             </div>
                             <div className="flex flex-col gap-1">
                                 <input onChange={(e) => handleChange('phone', e.target.value)} value={data.phone} className="text-black font-medium outline-none rounded-md py-2 px-3 bg-zinc-200" type="tel" id="tel" name="phone" maxLength={16} placeholder="WhatsApp: (00) 00000-0000" required />
+                            </div>
+                            <div className="flex flex-col gap-1">
+                                <input onChange={(e) => handleChange('cnpj', e.target.value)} value={data.cnpj} className="text-zinc-600 text-sm outline-none rounded-md py-2 px-3 bg-zinc-100" type="cnpj" name="cnpj" minLength={18} maxLength={18} placeholder="CNPJ" required />
+                                <p className="text-xs text-zinc-500 translate-x-3 text-left">Para CNPJ, siga o formato: 00.000.000/0001-00</p>
                             </div>
                             <button className="w-full text-sm sm:text-base text-center flex items-center justify-center gap-2 px-6 py-2 font-semibold text-white bg-[#01b013] hover:bg-[#01b013de] transition rounded-full" type="submit">
                                 {!loading ? (
